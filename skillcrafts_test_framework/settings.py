234ff965d312c9d25efdad1bb53e07096731bbec
vsevolodmartynyuk <vsevolodmartynyuk@yandex.ru>
2026-05-20 14:15:10 +0300

    Add STATIC_ROOT for collectstatic

--- skillcrafts_test_framework/settings.py
+++ skillcrafts_test_framework/settings.py
@@ -153,6 +153,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ---------- Статика (фронтенд) ----------
 STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'test_platform/static')]  # если статика лежит в приложении
 
 # ---------- CSRF для AJAX ----------
